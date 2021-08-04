e0b8ba12c4bcd32c951c13f9f8276b77dd110a45
Geraldo Istalin <geraldoboueres@gmail.com>
2021-08-03 23:33:19 -0300

    document.tsx

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -17,6 +17,12 @@ export const theme = extendTheme({
 
     },
 
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto',
+
+    },
+
     styles: {
         global: {
             body: {
