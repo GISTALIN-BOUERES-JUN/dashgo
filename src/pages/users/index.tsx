cfa8bde7f844f557eb2aa5b99d8c2825ee30ff04
Geraldo Istalin <geraldoboueres@gmail.com>
2021-08-07 20:14:14 -0300

    Responsive Users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,19 @@
-import { Heading, Box, Flex, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Heading, Box, Flex, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import React from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header"
-import { Pagination } from "../../components/pagination";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 
 
 export default function UserList() {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return (
         <Box>
             <Header />
@@ -32,17 +38,17 @@ export default function UserList() {
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de Cadastro</Th>
+                                {isWideVersion && <Th>Data de Cadastro</Th>}
                                 <Th width="8"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
                                 <Td>
@@ -51,7 +57,8 @@ export default function UserList() {
                                         <Text fontSize="sm" color="gray.300">gerladoboueres@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>05 de Agosto de 2021</Td>
+                                {isWideVersion && <Td>05 de Agosto de 2021</Td>}
+
                                 <Td>
                                     <Button
                                         as="a"
