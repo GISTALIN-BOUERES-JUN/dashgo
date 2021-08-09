dfde6caa43dcdf929aff7ad457bc43895eac321f
Geraldo Istalin <geraldoboueres@gmail.com>
2021-08-09 08:19:08 -0300

    React Query Devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,9 +14,28 @@ export default function UserList() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
-        return data;
 
-    })
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                }),
+            }
+
+
+
+        });
+        return users;
+
+    }, {
+        staleTime: 1000 * 5, //5 seconds
+    }
+    )
 
     console.log(isLoading);
 
@@ -86,31 +105,38 @@ export default function UserList() {
                                         </Tr>
                                     </Thead>
                                     <Tbody>
-                                        <Tr>
-                                            <Td px={["4", "4", "6"]}>
-                                                <Checkbox colorScheme="pink" />
-                                            </Td>
-                                            <Td>
-                                                <Box>
-                                                    <Text fontWeight="bold">Geraldo Boueres</Text>
-                                                    <Text fontSize="sm" color="gray.300">gerladoboueres@gmail.com</Text>
-                                                </Box>
-                                            </Td>
-                                            {isWideVersion && <Td>05 de Agosto de 2021</Td>}
+                                        {data.map(user => {
 
-                                            <Td>
-                                                <Button
-                                                    as="a"
-                                                    size="sm"
-                                                    fontSize="sm"
-                                                    colorScheme="purple"
-                                                    leftIcon={<Icon as={RiPencilLine} />}
-                                                >
-                                                    Editar
-                                                </Button>
+                                            return (
+                                                <Tr key={user.id}>
+                                                    <Td px={["4", "4", "6"]}>
+                                                        <Checkbox colorScheme="pink" />
+                                                    </Td>
+                                                    <Td>
+                                                        <Box>
+                                                            <Text fontWeight="bold">{user.name}</Text>
+                                                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                        </Box>
+                                                    </Td>
+                                                    {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                                            </Td>
-                                        </Tr>
+                                                    <Td>
+                                                        <Button
+                                                            as="a"
+                                                            size="sm"
+                                                            fontSize="sm"
+                                                            colorScheme="purple"
+                                                            leftIcon={<Icon as={RiPencilLine} />}
+                                                        >
+                                                            Editar
+                                                        </Button>
+
+                                                    </Td>
+                                                </Tr>
+                                            )
+
+                                        })}
+
 
                                     </Tbody>
 
